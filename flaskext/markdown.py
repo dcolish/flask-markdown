e99d1236323bb52b46cb17532638d9436b0b4bc5
Dan <dcolish@gmail.com>
2010-07-11 16:04:45 -0700

    init and push

---
+++ flaskext/markdown.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    flaskext.markdown
+    ~~~~~~~~~~~~~~~~~
+
+    Description of the module goes here...
+
+    :copyright: (c) 2010 by Dan Colish.
+    :license: BSD, see LICENSE for more details.
+"""
---
+++ setup.py
@@ -0,0 +1,44 @@
+"""
+Flask-Markdown
+--------------
+
+Description goes here...
+
+Links
+`````
+
+* `documentation <http://packages.python.org/Flask-Markdown>`_
+* `development version
+  <http://github.com/USERNAME/REPOSITORY/zipball/master#egg=Flask-Markdown-dev>`_
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-Markdown',
+    version='0.1',
+    url='<enter URL here>',
+    license='BSD',
+    author='Dan Colish',
+    author_email='your-email-here@example.com',
+    description='<enter short description here>',
+    long_description=__doc__,
+    packages=['flaskext'],
+    namespace_packages=['flaskext'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
