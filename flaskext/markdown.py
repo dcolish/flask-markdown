aa22e73c56b8631258d2dd00b7e671cb52f7d5fd
Dan <dcolish@gmail.com>
2010-07-12 16:58:37 -0700

    refactor for filter

--- flaskext/markdown.py
+++ flaskext/markdown.py
@@ -9,37 +9,13 @@
     :license: BSD, MIT see LICENSE for more details.
 """
 from __future__ import absolute_import
-import jinja2
-import jinja2.ext
-import markdown
-from flaskext.jinja2extender import extend_jinja
+from flask import current_app
+import markdown as md
 
 
-def load_markdown(app):
-    extend_jinja(app, [MarkdownExtension])
+def markdown(value):
+    return md.markdown(value)
 
 
-class MarkdownExtension(jinja2.ext.Extension):
-    tags = set(['markdown'])
-
-    def __init__(self, environment):
-        super(MarkdownExtension, self).__init__(environment)
-        environment.extend(
-            markdowner=markdown.Markdown(),
-        )
-
-    def parse(self, parser):
-        lineno = parser.stream.next().lineno
-        body = parser.parse_statements(
-            ['name:endmarkdown'],
-            drop_needle=True,
-        )
-        return jinja2.nodes.CallBlock(
-            self.call_method('_markdown_support'),
-            [],
-            [],
-            body,
-        ).set_lineno(lineno)
-
-    def _markdown_support(self, caller):
-        return self.environment.markdowner.convert(caller()).strip()
+def load_markdown():
+    current_app.jinja_env.filters.append(markdown)
